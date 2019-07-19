9bf0f88e34ee53a508775fe398191ae677e5d670
Tang Tianlong <tiaotiao_long@yeah.net>
2019-07-19 17:36:05 +0800

    添加了thinkphp3.2.3 sql注入漏洞

---
+++ thinkphp3wheresqlin/tp3/Application/Common/Conf/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    //添加数据库的配置信息
+    'DB_TYPE' => 'mysql' ,
+    'DB_HOST' => '127.0.0.1' ,
+    'DB_NAME' => 'thinkphp',
+    'DB_USER' => 'root',
+    'DB_PWD'  => 'root',
+    'DB_PORT' => '8889',
+    'DB_FIELDS_CACHE' => true,
+//    'DB_CHARSET' => 'utf-8',
+
+
+);
